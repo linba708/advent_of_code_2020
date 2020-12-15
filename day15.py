b676fe70eaada0d28740e3b6caacbac1375aafb7
Linus Back <linus@populum.se>
2020-12-15 18:51:55 +0100

    added alternate solution that is slightly faster

--- day15.py
+++ day15.py
@@ -1,7 +1,10 @@
-import functools
-import re
+import timeit
 
-# Part one
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+# Part one and two
 def part_one(numbers, stop):
     i = len(numbers) + 1
     spoken_numbers = {num: (j + 1, j + 1) for j, num in enumerate(numbers)}
@@ -14,11 +17,31 @@ def part_one(numbers, stop):
         else:
             spoken_numbers[to_speak] = (i,spoken_numbers[to_speak][0])
         previusly_spoken = spoken_numbers[to_speak]
-        if (i % 250000 == 0):
+        if (i % 5000000 == 0):
             print(i)
         i += 1
     print(to_speak)
-    
+
+def alternative(numbers, stop):
+    i = len(numbers) + 1
+    spoken_arr = [None]*stop
+    for j, num in enumerate(numbers):
+        spoken_arr[num] = (j + 1, j + 1)
+    to_speak = numbers[-1]
+    previusly_spoken = spoken_arr[to_speak]
+    while i < stop + 1:
+        to_speak = previusly_spoken[0] - previusly_spoken[1]
+        if spoken_arr[to_speak] is None:
+            spoken_arr[to_speak] = (i, i)
+            previusly_spoken = (i, i)
+        else:
+            j = spoken_arr[to_speak][0]
+            spoken_arr[to_speak] = (i,j)
+            previusly_spoken = (i,j)
+        if (i % 5000000 == 0):
+            print('loop nr:',i)
+        i += 1
+    print(to_speak)    
 
 numbers = []
 #read data
@@ -29,6 +52,10 @@ with open('data/day15.txt') as reader:
 testdict = {1: 'hello', 2: 'world'}
 testdict[3] = []
 print(testdict[3])
-part_one(numbers, 2020)
-
-part_one(numbers, 30000000)
+# part_one(numbers, 2020)
+wrapped = wrapper(part_one, numbers, 2020)
+print(timeit.timeit(wrapped,number=1))
+wrapped = wrapper(part_one, numbers, 30000000)
+print(timeit.timeit(wrapped,number=1))
+wrapped = wrapper(alternative, numbers, 30000000)
+print(timeit.timeit(wrapped,number=1))
