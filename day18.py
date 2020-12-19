e3f6c31e048af5a8bca4e0657be5f9bdb2740835
Linus Back <linus@populum.se>
2020-12-19 02:30:12 +0100

    day 18 and start of day 19

--- day18.py
+++ day18.py
@@ -1,3 +1,4 @@
+import functools
 
 #part one
 def eval_list(expressions, acc):
@@ -22,30 +23,37 @@ def eval_list(expressions, acc):
     return i, acc
 
 
-def eval_list_advance(expressions, acc):
+def eval_list_advance(expressions, result_list):
     last_operator = ''
     # print(expressions)
     i = 0
+    last_number = 0
     while i < len(expressions):
         e = expressions[i]
         if e == '(':
-            j,e = eval_list(expressions[i +1:], 0)
+            j,e = eval_list_advance(expressions[i +1:], [])
             i += j
             # print('incremented', i, e )
         elif e == ')':
-            # print('returning', acc)
-            return (i + 1), acc
-        # print(acc, last_operator)
-        if e in ('*', '+'):
+            result_list.append(last_number)
+            # print('returning', result_list)
+            return (i + 1), functools.reduce(lambda a,b: a*b, result_list)
+        # print(result_list, last_number,last_operator, e)
+        if e == '+':
             last_operator = e
+        elif e == '*':
+            last_operator = e
+            result_list.append(last_number)
         elif last_operator == '*':
-            acc *= int(e)
+            last_number = int(e)
         elif last_operator == '+':
-            acc += int(e)
+            last_number += int(e)
         else:
-            acc = int(e)
+            last_number = int(e)
         i += 1
-    return i, acc
+        if i == len(expressions):
+            result_list.append(last_number)
+    return i, functools.reduce(lambda a,b: a*b, result_list)
 
 
 
@@ -53,7 +61,7 @@ results = 0
 with open('data/day18.txt') as data:
     for y, line in enumerate(data.readlines()):
         list = line.strip().replace('(', '( ').replace(')', ' )').split(' ')
-        _,result = eval_list(list, 0)
+        _,result = eval_list_advance(list, [])
         results += result
         print('result',result)
         
---
+++ day19.py
@@ -0,0 +1,7 @@
+
+with open('data/day19.txt') as data:
+    for line in data.readlines():
+        line = line.strip()
+        print(line)
+        
+
